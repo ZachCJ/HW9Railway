ef6111fe2e1418af40d89a78d96844d803af715c
Zachary Johnston <ZacharyJohnston324@u.boisestate.edu>
2025-12-18 16:29:20 -0700

    LAST FIX I hope

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -59,7 +59,7 @@ public class GettingStartedApplication {
             statement.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
 
 
-            final var resultSet = statement.executeQuery("SELECT random_string FROM table_timestamp_and_random_string");
+            final var resultSet = statement.executeQuery("SELECT tick, random_string FROM table_timestamp_and_random_string");
             final var output = new ArrayList<>();
             while (resultSet.next()) {
                 output.add("Read from DB: " + resultSet.getTimestamp("tick") + resultSet.getString("random_string"));
